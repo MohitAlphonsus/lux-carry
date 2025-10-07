c503c5bd741d62bc04280b3ca0512b4fd0bc8559
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-07 15:45:34 +0530

    image path fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,24 @@
-import { Button } from '../components';
+import { useNavigate } from 'react-router';
+import { Logo, Button } from '../components';
 import styles from './Home.module.css';
 
 import { heroImgs, heroFeatures } from '../constants/constant';
 
 export default function Home() {
+	const navigate = useNavigate();
+
 	return (
 		<section className={styles.hero}>
+			<Logo />
 			<div className={styles.heroTextBox}>
 				<h1>Carry Elegance Everywhere</h1>
 				<p>
 					Discover premium bags crafted to perfection. Where style meets
 					sophistication, every bag tells a story.
 				</p>
-				<Button priority="SECONDARY">Find Your Perfect Bag</Button>
+				<Button priority="SECONDARY" onClick={() => navigate('/product-store')}>
+					Find Your Perfect Bag
+				</Button>
 				<div className={styles.heroFeatures}>
 					{heroFeatures.map(({ id, icon: Icon, feature }) => (
 						<div className={styles.feature} key={id}>
