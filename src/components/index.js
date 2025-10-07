bb0df8a9764061a825144f0a40453eeddf7c28c8
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-07 15:43:10 +0530

    central components imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Home from './Home';
 import Button from './Button';
 import Logo from './Logo';
+import PageTitle from './PageTitle';
+import Products from './Products';
 
-export { Logo, Home, Button };
+export { Logo, Home, Button, PageTitle, Products };
