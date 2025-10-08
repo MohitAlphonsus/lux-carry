23efb085c65600c8cd9d33593f5c330d89f2b8f1
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-08 18:14:41 +0530

    product details page when clicked on single product

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { useProducts } from '../context/ProductsContext';
+import styles from './ProductDetails.module.css';
+import { Button, Counter, Tags } from '../components';
+import { FaCheckCircle } from 'react-icons/fa';
+
+export default function ProductDetails() {
+	const [mainShowImg, setMainShowImg] = useState('');
+	const { id } = useParams();
+	const { product, isLoading, onFetchDetailsById } = useProducts();
+
+	function handleImageSwitch(srcImg) {
+		setMainShowImg(srcImg);
+	}
+
+	useEffect(function () {
+		onFetchDetailsById(id);
+	}, []);
+
+	if (isLoading) return <p>Loading...</p>;
+	if (!product) return <p>Data not found</p>;
+
+	return (
+		<div className="container">
+			<section className={styles.productDetails}>
+				<div className={styles.productImages}>
+					<div className={styles.stackOfProductImages}>
+						{product?.images?.map(img => (
+							<div
+								className={styles.stackImage}
+								style={{
+									backgroundImage: `url(${img.imgSrc})`,
+								}}
+								key={img.id}
+								onClick={() => handleImageSwitch(img.imgSrc)}
+							>
+								&nbsp;
+							</div>
+						))}
+					</div>
+					<div
+						className={styles.productImage}
+						style={{
+							backgroundImage: `url(${
+								mainShowImg === '' ? product?.images?.[0]?.imgSrc : mainShowImg
+							})`,
+						}}
+					>
+						&nbsp;
+					</div>
+				</div>
+				<div className={styles.productInformation}>
+					<h2>{product?.name}</h2>
+					<div>{product?.ratings}</div>
+					<Tags
+						productClosureType={product?.ClosureType}
+						productOuterMaterial={product?.OuterMaterial}
+						productStyle={product?.Style}
+					/>
+
+					<div className={styles.aboutDim}>
+						<p>
+							<span>Weight </span>
+							<span> {product?.Weight}g</span>
+						</p>
+						<p>
+							<span>Dimensions </span>
+							<span> {product?.dimensions}</span>
+						</p>
+					</div>
+
+					<ul className={styles.aboutList}>
+						{product?.about?.map(abt => (
+							<li key={abt}>
+								<FaCheckCircle />
+								<span>{abt}</span>
+							</li>
+						))}
+					</ul>
+					<div className={styles.footer}>
+						<p className={styles.price}>
+							<span>{product.price}</span>
+							<span>₹</span>
+						</p>
+						<Counter />
+						<Button>Add To Cart</Button>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
+}
